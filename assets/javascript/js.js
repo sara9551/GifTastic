ee7d13abdfbe7a60458de00cd20e027bb220c42e
Sara Anspach <sara.anspach@gmail.com>
2018-08-29 12:21:55 -0400

    third, changed because of 03,13

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,50 +1,30 @@
-$(document).ready(function () {
+    var topics = ["ice-cream", "sushi", "hamburguer", "eggs", "cereal", "pasta", "cake"];
 
-    var topics = ["ice-cream", "sushi", "hamburguer", "eggs", "yogurt", "cereal", "pasta", "cake", "banana", "apple", "burrito"];
-
-    function displayAnimalGiphys() {
-
-        var giphy = $(this).attr("data-food"); //-->>>>remember to use data-food
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + "U99VkwNS5zjeLiCzbwmkdXP2bdjBMNY4";
-
+    $("button").on("click", function() {
+        var food = $(this).attr("data-person");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=U99VkwNS5zjeLiCzbwmkdXP2bdjBMNY4&limit=10"; // --> LIMIT TO 10 GIFS
+  
         $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            console.log(queryURL);
-            console.log(response);
-
-            function renderButtons() {
-
-                $("#buttons-view").empty();
-
-                for (var i = 0; i < food.length; i++) {
-                    var a = $("<button>");
-
-                    a.addClass("food-btn");
-
-                    a.attr("data-food", food[i]);
-
-                    a.text(food[i]);
-
-                    $("#buttons-view").append(a);
-                }
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            var results = response.data;
+  
+            for (var i = 0; i < results.length; i++) {
+              var giphyDiv = $("<div class='item'>");
+  
+              var rating = results[i].rating;
+  
+              var p = $("<p>").text("Rating: " + rating);
+  
+              var foodImage = $("<img>");
+              foodImage.attr("src", results[i].images.fixed_height.url);
+  
+              giphyDiv.prepend(p);
+              giphyDiv.prepend(foodImage);
+  
+              $("#gifs-appear-here").prepend(giphyDiv);
             }
-
-            $("#add-food").on("click", function(event) {
-                event.preventDefault();
-
-                var food = $("#food-input").val().trim();
-        
-                topics.push(food);
-
-                console.log(food);
-
-
-            renderButtons();
-        });
-
-        $(document).on("click", ".giphy-btn", displaytopics);
-
-        renderButtons();
+          });
+      });
